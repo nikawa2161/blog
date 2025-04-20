dc51635c320cb9df0b4889af69dba9e3f7609c7e
nikawa2161 <nikawa.2161.t@gmail.com>
2025-04-20 23:54:30 +0900

    mdxの設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["md", "mdx", "ts", "tsx"],
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "@/posts": path.resolve(__dirname, "./src/posts"),
+      },
+    },
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // 必要に応じてremark・rehypeプラグインを追加
+});
+
+export default withMDX(nextConfig);
