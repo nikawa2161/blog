52d841b5309b20c76ae5b8d673a6b09757493bab
nikawa2161 <nikawa.2161.t@gmail.com>
2026-01-02 18:17:45 +0900

    Revert "feat: Open GraphとTwitter Cardのメタデータを追加"
    This reverts commit b2cec28cb1b8740efa924fc56b481faff9908658.

--- src/constants/SiteConfig/index.ts
+++ src/constants/SiteConfig/index.ts
@@ -1,8 +1,5 @@
 export const SiteConfig = {
 	name: "{{ nikawa2161 }}",
 	description: "日々の学び・気づき・試行錯誤を記録する個人ブログ。技術、仕事、暮らしのメモを中心に更新します。",
-	siteUrl: "https://www.nikawa2161.com",
-	githubUrl: "https://github.com/nikawa2161/blog",
-	// 後方互換性のため残す
 	url: "https://github.com/nikawa2161/blog",
 } as const;
